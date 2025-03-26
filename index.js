1bfa3c6eeecbb89db2c918124421cbcd7c307a38
Elisheva <e17171767@gmail.com>
2025-03-26 17:08:10 +0200

    add end point

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.get('', 'hellow')
 // Endpoint ל-GET שמחזיר את רשימת האפליקציות
 app.get('/apps', (req, res) => {
     const apps = [
